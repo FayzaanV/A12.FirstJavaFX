f0c9c974a595441f0e6699bc9ae5923aa3ef33aa
FayzaanV <fayzaanvirk6@yahoo.com>
2026-04-18 16:17:56 -0500

    Finished the code for A12

--- FirstJavaFX/src/first_javafx/App.java
+++ FirstJavaFX/src/first_javafx/App.java
@@ -2,7 +2,6 @@ package first_javafx;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -12,20 +11,20 @@ import javafx.scene.shape.Polygon;
 public class App extends Application {
     @Override
     public void start(Stage window) {
-        System.out.println("app started");
+        Label line1 = new Label("If debugging is the process of removing software bugs,");
+        Label line2 = new Label("then programming must be the process of putting them in.");
+        Label line3 = new Label("-- Edsger W. Dijkstra");
+        line2.setLayoutY(25);
+        line3.setLayoutY(50);
 
-        Label label = new Label("Click this button: ");
-        Button button = new Button("Click me!");
-        button.setLayoutY(25);
+        Circle circle = new Circle(160, 120, 60);
+        Polygon triangle = new Polygon(160, 120, 220, 280, 100, 280);
 
-        Circle circle = new Circle(200, 200, 20);
-        Polygon triangle = new Polygon(100, 150, 180, 90, 35, 80);
-
-        Group group = new Group(label, button, circle, triangle);
-        Scene scene = new Scene(group, 800, 600);
+        Group group = new Group(line1, line2, line3, circle, triangle);
+        Scene scene = new Scene(group, 480, 320);
 
         window.setScene(scene);
-        window.setTitle("JavaFXApp");
+        window.setTitle("CS400");
         window.show();
     }
 
